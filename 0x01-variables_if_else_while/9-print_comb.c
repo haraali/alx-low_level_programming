3f03b2bf86d7873ca415c447b6667542fbbb4275
jewharo ali <jowharaali8@gmail.com>
2023-03-17 12:52:01 -0700

    task 9 again

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
 		putchar(i + '0');
+		if (i == 9)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
