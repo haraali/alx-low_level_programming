73399432f9be7006349660f426424b40b5985983
jewharo ali <jowharaali8@gmail.com>
2023-05-08 11:10:23 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,22 +16,23 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		perror("open");
 		return (-1);
 	}
-	len = (text_content != NULL) ? strlen(text_content) : 0;
-	if (len > 0)
+	w = write(fd, text_content, len);
+	if (w == -1)
 	{
-		w = write(fd, text_content, len);
-		if (w == -1)
-		{
-			perror("write");
-			close(fd);
-			return (-1);
-		}
+		perror("write");
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
