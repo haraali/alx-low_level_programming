5269d1f5b527519bd2fa5d76466164e12ddaff37
jewharo ali <jowharaali8@gmail.com>
2023-05-08 11:14:51 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,14 +24,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		perror("open");
 		return (-1);
 	}
 	w = write(fd, text_content, len);
 	if (w == -1)
 	{
-		perror("write");
-		close(fd);
 		return (-1);
 	}
 	close(fd);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
